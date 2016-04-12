c3a781b42805188690101444a8ab23c9edf30463
Myles Noton <myles@mylesnoton.com>
2016-04-13 00:12:08 +0100

    Fixing how it works, removing unused params

--- gen.go
+++ gen.go
@@ -2,26 +2,26 @@ package passgen
 
 import (
 	"crypto/rand"
-	//"fmt"
 	"errors"
+	//"fmt"
 	"log"
 	"math/big"
 	"time"
 )
 
-func NewPassword(length int, use_numbers bool, use_special bool) (string, error) {
-	return generate(length, use_numbers, use_special)
+func NewPassword(length int) (string, error) {
+	return generate(length)
 }
 
-func generate(length int, use_numbers bool, use_special bool) (string, error) {
+func generate(length int) (string, error) {
 	start := time.Now()
 
 	password := ""
-	char := []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&'}
-	//special := []string{"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", ",", "+", "-", ".", "/", "_", "[", "]", ":", ";", "<", ">", "=", "@", "?", "{", "}", "|", "~", "^"}
+	char := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*,+-./_[]:;<>=@?{}|~^")
 
-	for _, value := range char {
-		password += string(value)
+	for i := 0; i < length; i++ {
+		num, _ := generateRandomNumber()
+		password += string(char[num])
 	}
 
 	elapsed := time.Since(start)
@@ -30,8 +30,8 @@ func generate(length int, use_numbers bool, use_special bool) (string, error) {
 }
 
 func generateRandomNumber() (int64, error) {
-	//max := *big.NewInt(92)
-	max := *big.NewInt(26)
+	max := *big.NewInt(92)
+
 	n, err := rand.Int(rand.Reader, &max)
 
 	if err != nil {
