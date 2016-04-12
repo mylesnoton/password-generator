232b86eeb733d967095bc8a3644cbc0408635576
Myles Noton <myles@mylesnoton.com>
2016-04-13 00:07:45 +0100

    Added more in depth unit tests that now check for existence of upper, lower, number and special chars, as well as test length and type of randomnumber

--- gen_test.go
+++ gen_test.go
@@ -4,10 +4,94 @@ import (
 	"testing"
 )
 
-func TestGenerate(t *testing.T) {
-	password := generate(10)
+func TestNewPassword(t *testing.T) {
+	password, _ := NewPassword(10, true, true)
 
 	if len(password) != 10 {
+		t.Fail()
+	}
+}
+
+func TestNewPasswordHasLowerChar(t *testing.T) {
+	password, _ := NewPassword(10, true, true)
+
+	lowers := []byte("abcdefghijklmnopqrstuvwxyz")
+	found := false
+
+	for _, find_char := range lowers {
+		for _, password_char := range password {
+			if string(find_char) == string(password_char) {
+				found = true
+			}
+		}
+	}
+
+	if found != true {
+		t.Fail()
+	}
+}
+
+func TestNewPasswordHasUpperChar(t *testing.T) {
+	password, _ := NewPassword(10, true, true)
+
+	uppers := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	found := false
+
+	for _, find_char := range uppers {
+		for _, password_char := range password {
+			if string(find_char) == string(password_char) {
+				found = true
+			}
+		}
+	}
+
+	if found != true {
+		t.Fail()
+	}
+}
+
+func TestNewPasswordHasSpecialChar(t *testing.T) {
+	password, _ := NewPassword(10, true, true)
+
+	specials := []byte("!\"#$%&'()*,+-./_[]:;<>=@?{}|~^")
+	found := false
+
+	for _, find_char := range specials {
+		for _, password_char := range password {
+			if string(find_char) == string(password_char) {
+				found = true
+			}
+		}
+	}
+
+	if found != true {
+		t.Fail()
+	}
+}
+
+func TestNewPasswordHasNumber(t *testing.T) {
+	password, _ := NewPassword(10, true, true)
+
+	numbers := []byte("0123456789")
+	found := false
+
+	for _, find_number := range numbers {
+		for _, password_char := range password {
+			if int(find_number) == int(password_char) {
+				found = true
+			}
+		}
+	}
+
+	if found != true {
+		t.Fail()
+	}
+}
+
+func TestGenerateRandomNumber(t *testing.T) {
+	number, _ := generateRandomNumber()
+
+	if number != int64(number) {
 		t.Fail()
 	}
 }
